50b033f8b200b61ee17977bba4a6af8adbb47426
Ivanko <52831330+ivanko2k@users.noreply.github.com>
2019-11-24 11:21:44 +0700

    test

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     long_description_content_type='text/markdown',
     author='Deezer Research',
     author_email='research@deezer.com',
-    url='https://github.com/deezer/spleeter',
+    url='https://github.com/ivanko2k/New',
     license='MIT License',
     packages=[
         'spleeter',
